3fa2b3c299394ca96601fb9af7f52477297fc7f7
水秋玄 <44487177+mkinit@users.noreply.github.com>
2021-04-22 18:39:37 +0800

    Update request.js

--- request/request.js
+++ request/request.js
@@ -1,48 +1,41 @@
 import axios from 'axios'
-export default ({
-	base_url = '',
-	success,
-	error
-}) => {
-	/*
-	 *生产环境的api地址。
-	 *vue.config.js 同时设置代理地址
-	 module.exports = {
-		devServer: {
-			proxy: 'https://tp6.com/api.blog/'
-		}
-	 }
-	 */
-	if (process.env.NODE_ENV !== 'development') {
-		axios.defaults.baseURL = base_url
+
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+axios.defaults.baseURL = dev ? '' : 'https://js2api.com/js2-json/v1/'
+
+/*
+ * 跨域代理
+ * vue.config.js 
+ module.exports = {
+	devServer: {
+		proxy: 'https://tp6.com/api.blog/'//生产环境api地址，开启代理时，axios.defaults.baseURL必须为空
 	}
+ }
+ */
 
-	//请求拦截
-	axios.interceptors.request.use(request => {
-		request.headers = {
-			token:localStorage.getItem('token'),
-			...request.headers
-		}
-		return request
-	}, error => {
-		return new Promise.reject(error)
-	})
+//请求拦截
+axios.interceptors.request.use(request => {
+	request.headers = {
+		...request.headers
+	}
+	if (localStorage.getItem('token') || sessionStorage.getItem('token')) {
+		request.headers.token = localStorage.getItem('token')
+	}
+	return request
+}, error => {
+	return new Promise.reject(error)
+})
 
-	//响应拦截
-	axios.interceptors.response.use(res => {
-		//成功，HTTP状态：200
-		//必须返回一个新的promise，否则这里走的通道都是then
-		return new Promise((resolve, reject) => {
-			success(resolve,reject,res)
-		})
+//响应拦截
+axios.interceptors.response.use(res => {
+	//成功，HTTP状态：200
+	return Promise.resolve(res)
+}, err => {
+	//失败，HTTP状态：200以外的
+	console.log('状态码：', err.response.data.code)
+	console.log('自定义错误信息：', err.response.data.msg)
+	return Promise.reject(err)
+})
 
-	}, err => {
-		//失败，HTTP状态：200以外的
-		error(err)
-		//直接return走的是then通道，需要Promise.reject才会走catch通道
-		return Promise.reject(err)
-	})
-
-
-	return axios
-}
+export default axios
